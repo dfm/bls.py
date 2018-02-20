0f8c803517e97fe3b2ed84e7f0fb67c173c3ec27
Dan F-M <danfm@nyu.edu>
2018-02-20 16:33:01 -0500

    docstrings

--- transit_periodogram.py
+++ transit_periodogram.py
@@ -22,6 +22,8 @@ def strip_units(*arrs):
 
 
 class TransitPeriodogram(object):
+    """
+    """
 
     def __init__(self, t, y, dy=None):
         self.t, self.y, self.dy = self._validate_inputs(t, y, dy)
@@ -106,11 +108,74 @@ class TransitPeriodogram(object):
                                          depth_err, transit_time, duration,
                                          depth_snr, log_likelihood)
 
-    def autoperiod(self, duration, minimum_n_transit=3, frequency_factor=1.0,
-                   minimum_period=None, maximum_period=None):
+    def _time_unit(self):
+        if has_units(self.t):
+            return self.t.unit
+        else:
+            return 1
+
+    def autoperiod(self, duration,
+                   minimum_period=None, maximum_period=None,
+                   minimum_n_transit=3, frequency_factor=1.0):
+        """Determine a suitable grid of periods
+
+        This method uses a set of heuristics to select a conservative period
+        grid that is uniform in frequency. This grid might be too fine for
+        some user's needs depending on the precision requirements or the
+        sampling of the data. The grid can be made coarser by increasing
+        ``frequency_factor``.
+
+        Parameters
+        ----------
+        duration : array-like or Quantity
+            The set of durations that will be considered.
+        minimum_period, maximum_period : float or Quantity, optional
+            The minimum/maximum periods to search. If not provided, these will
+            be computed as described in the notes below.
+        minimum_n_transits : int, optional
+            If ``maximum_period`` is not provided, this is used to compute the
+            maximum period to search by asserting that any systems with at
+            least ``minimum_n_transits`` will be within the range of searched
+            periods. Note that this is not the same as requiring that
+            ``minimum_n_transits`` be required for detection. The default
+            value is ``3``.
+        frequency_factor : float, optional
+            A factor to control the frequency spacing as described in the
+            notes below. The default value is ``1.0``.
+
+        Returns
+        -------
+        period : array-like or Quantity
+            The set of periods computed using these heuristics with the same
+            units as ``t``.
+
+        Notes
+        -----
+        The default minimum period is chosen to be twice the maximum duration
+        because there won't be much sensitivity to periods shorter than that.
+
+        The default maximum period is computed as
+
+        .. code-block:: python
+
+            maximum_period = (max(t) - min(t)) / minimum_n_transits
+
+        ensuring that any systems with at least ``minimum_n_transits`` are
+        within the range of searched periods.
+
+        The frequency spacing is given by
+
+        .. code-block:: python
+
+            df = frequency_factor * min(duration) / (max(t) - min(t))**2
+
+        so the grid can be made finer by decreasing ``frequency_factor`` or
+        coarser by increasing ``frequency_factor``.
+
+        """
         duration = self._validate_duration(duration)
-        baseline = self.t.max() - self.t.min()
-        min_duration = np.min(duration)
+        baseline = strip_units(self.t.max() - self.t.min())
+        min_duration = strip_units(np.min(duration))
 
         # Estimate the required frequency spacing
         # Because of the sparsity of a transit, this must be much finer than
@@ -123,16 +188,46 @@ class TransitPeriodogram(object):
         # maximum duration because we won't be sensitive to any periods
         # shorter than that.
         if minimum_period is None:
-            minimum_period = 2.0 * np.max(duration)
+            minimum_period = 2.0 * strip_units(np.max(duration))
+        else:
+            if has_units(self.t):
+                minimum_period = units.Quantity(minimum_period)
+                try:
+                    minimum_period = units.Quantity(minimum_period,
+                                                    unit=self.t.unit)
+                except units.UnitConversionError:
+                    raise ValueError("Units of minimum_period not equivalent "
+                                     "to units of t")
+            else:
+                if has_units(minimum_period):
+                    raise ValueError("minimum_period has units while t "
+                                     "doesn't.")
 
         # If no maximum period is provided, choose one by requiring that
         # all signals with at least minimum_n_transit should be detectable.
         if maximum_period is None:
             maximum_period = baseline / minimum_n_transit
+        else:
+            if has_units(self.t):
+                maximum_period = units.Quantity(maximum_period)
+                try:
+                    maximum_period = units.Quantity(maximum_period,
+                                                    unit=self.t.unit)
+                except units.UnitConversionError:
+                    raise ValueError("Units of maximum_period not equivalent "
+                                     "to units of t")
+            else:
+                if has_units(maximum_period):
+                    raise ValueError("maximum_period has units while t "
+                                     "doesn't.")
+
+        # Convert bounds to frequency
+        minimum_frequency = 1.0/strip_units(maximum_period)
+        maximum_frequency = 1.0/strip_units(minimum_period)
 
         # Compute the number of frequencies and the frequency grid
-        nf = 1 + int(np.round((1.0/minimum_period - 1.0/maximum_period)/df))
-        return 1.0 / (1.0 / minimum_period - df * np.arange(nf))
+        nf = 1 + int(np.round((maximum_frequency - minimum_frequency)/df))
+        return 1.0/(maximum_frequency-df*np.arange(nf)) * self._time_unit()
 
     def autopower(self, duration, objective=None, method=None, oversample=10,
                   pool=None, minimum_n_transit=3, minimum_period=None,
@@ -146,14 +241,54 @@ class TransitPeriodogram(object):
 
     def power(self, period, duration, objective=None, method=None,
               oversample=10, pool=None):
+        """Compute the periodogram for a set of periods
+
+        Parameters
+        ----------
+        period : array-like or Quantity
+            The periods where the power should be computed
+        duration : array-like or Quantity
+            The set of durations to test
+        objective : {'likelihood', 'snr'}, optional
+            The scalar that should be optimized to find the best fit phase,
+            duration, and depth. This can be either ``'likelihood'`` (default)
+            to optimize the log-likelihood of the model, or ``'snr'`` to
+            optimize the signal-to-noise with which the transit depth is
+            measured.
+        method : {'fast', 'fast_python', 'slow'}, optional
+            The computational method used to compute the periodogram. This is
+            mainly included for the purposes of testing and most users will
+            want to use the optimized ``'fast'`` method (default) that is
+            implemented in Cython. The ``'fast_python'`` method is an
+            implementation of the ``'fast'`` method in pure Python and
+            ``'slow'`` is a brute-force method that is used to test the
+            results of the other methods.
+        oversample : int, optional
+            The number of bins per duration that should be used. This sets the
+            time resolution of the phase fit with larger values of
+            ``oversample`` yielding a finer grid and higher computational cost.
+
+        Returns
+        -------
+        results : TransitPeriodogramResults
+            The periodogram results as a :class:`TransitPeriodogramResults`
+            object.
+
+        Raises
+        ------
+        ValueError
+            If ``oversample`` is not an integer greater than 0 or if
+            ``objective`` or ``method`` are not valid.
+
+        """
         period, duration = self._validate_period_and_duration(period, duration)
 
         # Check for absurdities in the ``oversample`` choice
         try:
             oversample = int(oversample)
         except TypeError:
-            raise ValueError("oversample must be an integer,"
-                             " got {0}".format(oversample))
+            raise ValueError("oversample must be an int, got {0}"
+                             .format(oversample))
         if oversample < 1:
             raise ValueError("oversample must be greater than or equal to 1")
 
